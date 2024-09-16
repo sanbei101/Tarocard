fb60a83737b076c6437b6780554855588d6679ab
sanbei101 <sanbei101@outlook.com>
2024-09-16 03:52:29 +0000

    3

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,40 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
+import viteCompression from 'vite-plugin-compression';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    viteCompression({
+      verbose: true, // 输出压缩结果
+      disable: false, // 是否禁用插件
+      threshold: 10240, // 只有大于该值的资源会被压缩，单位是字节
+      algorithm: "gzip", // 压缩算法
+      ext: ".gz", // 生成的压缩文件的后缀
+    }),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "vue-bundle": ["vue"],
+          "naive-ui-bundle": ["naive-ui"],
+        },
+        entryFileNames: "assets/[name].[hash].js",
+        chunkFileNames: "assets/[name].[hash].js",
+        assetFileNames: "assets/[name].[hash].[ext]",
+      },
+      external: ["fsevents"],
+    },
+
+    minify: "terser", // 确保 Vite 使用 terser 进行代码压缩
+    terserOptions: {
+      compress: {
+        drop_console: true, // 去掉所有的 console.log 调用
+        drop_debugger: true, // 去掉所有的 debugger 语句
+      },
+    },
+  },
 });
