7fcb8a0c6f9a55545d7989a3551bc25affed447e
sanbei101 <sanbei101@outlook.com>
2024-09-17 15:12:11 +0000

    12

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       verbose: true, // 输出压缩结果
       disable: false, // 是否禁用插件
       threshold: 10240, // 只有大于该值的资源会被压缩，单位是字节
-      algorithm: "gzip", // 压缩算法
+      algorithm: "gzip",
       ext: ".gz",
     }),
   ],
