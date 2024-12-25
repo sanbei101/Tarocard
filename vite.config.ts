f4cea0da543fed03c5f105de5e124c1183cba8a3
sanbei101 <sanbei101@outlook.com>
2024-12-25 19:58:26 +0800

    分包编译

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,31 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteCompression({
+      verbose: true,
+      disable: false,
+      threshold: 10240,
+      algorithm: 'gzip',
+      ext: '.gz'
+    })
+  ],
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vue-bundle': ['vue'],
+          'naive-ui-bundle': ['naive-ui']
+        },
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      },
+      external: ['fsevents']
+    }
+  }
 });
